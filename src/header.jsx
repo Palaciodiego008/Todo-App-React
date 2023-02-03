e405f58129abc77d576ce02dc997dd46fcf672b8
Diego Palacio <palaciodiego099@gmail.com>
2023-02-03 11:00:30 -0500

    Added header to the main app and setting again tailwind to react

---
+++ src/header.jsx
@@ -0,0 +1,19 @@
+function Header() {
+    return (
+        <header>
+            <div className="bg-cyan-500 pb-10">
+                <div className="flex flex-col">
+                    <div className="flex flex-row">
+                        <h1 className="text-white font-medium text-lg mt-7 ml-5 text-left"> ToDo List </h1>
+                       
+                    </div>    
+                 
+             
+
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
