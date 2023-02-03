0d286635ff8598f96f9f2f66834ecad705e540d1
Diego Palacio <palaciodiego099@gmail.com>
2023-02-03 13:51:38 -0500

    Adding fake list

--- src/Views/list.jsx
+++ src/Views/list.jsx
@@ -1,9 +1,8 @@
 function List() {
     return (
-
             <div className="py-3 px-8 justify-center flex flex-col">
-                <table className="divide-y  bg-cyan-900">
-                <thead>
+                <table className="divide-y">
+                <thead className="bg-cyan-900">
                   <tr>
                     <th scope="col" className="px-6 py-6 text-left text-xs font-medium text-sixth-500 uppercase tracking-wider text-white">
                       Task
@@ -15,18 +14,37 @@ function List() {
   
                     <th scope="col" className="px-6 py-6 text-center text-xs font-medium text-sixth-500 uppercase tracking-wider text-white">
                       Actions
-                    </th>
-  
-  
+                    </th>  
                   </tr>
                 </thead>
-                <tbody className="divide-y divide-sixth-200">
-                
-    
+                <tbody className="divide-y divide-sixth-200 shadow bg-white">
+                  <tr>
+                    <td className="px-4 py-3 whitespace-nowrap text-sm text-white">
+                      <div className="flex items-left">
+                        <div className="flex">
+                          <div className="text-sm font-medium text-cyan-400">
+                            Task 1
+                          </div>
+                        </div>
+                      </div>
+                    </td>
+                  </tr>
+
+                  <tr>
+                    <td className="px-4 py-3 whitespace-nowrap text-sm text-white">
+                      <div className="flex items-left">
+                        <div className="flex">
+                          <div className="text-sm font-medium text-cyan-400">
+                            Task 1
+                          </div>
+                        </div>
+                      </div>
+                    </td>
+                  </tr>
+
   
                 </tbody>
-              </table>
-            
+              </table>  
             </div>
       
     );
