e3eb0445f3ac41755452a0001a4d579ca38ccf7b
Diego Palacio <palaciodiego099@gmail.com>
2023-02-03 11:06:17 -0500

    Adding subdirectory

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import Header  from './header';
+import Header  from './Layouts/header';
 
 function App() {
   return (
--- src/Layouts/header.jsx
+++ src/Layouts/header.jsx
@@ -5,11 +5,7 @@ function Header() {
                 <div className="flex flex-col">
                     <div className="flex flex-row">
                         <h1 className="text-white font-medium text-lg mt-7 ml-5 text-left"> ToDo List </h1>
-                       
                     </div>    
-                 
-             
-
                 </div>
             </div>
         </header>
